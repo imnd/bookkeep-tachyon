0be0f6cda87d629adee7697a6558827236144a04
imnd <imndsk@gmail.com>
2019-11-26 09:23:27 +0700

    add style

--- app/views/layouts/list.php
+++ app/views/layouts/list.php
@@ -1,5 +1,6 @@
 @extends('crud')
 
 <link rel="stylesheet" href="/assets/widgets/grid/style.css" type="text/css" media="screen">
+<script type="text/javascript" src="/assets/widgets/grid/sort.js"></script>
 
 @contents

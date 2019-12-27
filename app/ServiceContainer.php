17e430a435fd1670e9e0cae0b9294c33a3d42bd6
Anderw Serdjuk <imndsu@gmail.com>
2019-12-27 23:55:12 +0700

    refactoring of ServiceContainer

--- app/ServiceContainer.php
+++ app/ServiceContainer.php
@@ -1,8 +1,9 @@
 <?php
 namespace app;
 
-use tachyon\Request;
-use tachyon\dic\Container;
+use tachyon\Request,
+    tachyon\dic\Container,
+    tachyon\traits\ClassName;
 
 /**
  * Dependency Injection Container
@@ -12,12 +13,14 @@ use tachyon\dic\Container;
  */
 class ServiceContainer extends Container
 {
+    use ClassName;
+
     public function boot()
     {
-        $controllerClass = Request::get('controller');
-        $controllerName = substr($controllerClass, strrpos($controllerClass, '\\') + 1);
+        $controllerName = $this->getClassName(Request::get('controller'));
         $entity = str_replace('Controller', '', $controllerName);
         // сопоставление интерфейсов зависимостей с реализацией
         $this->implementations['app\interfaces\RepositoryInterface'] = "app\\repositories\\{$entity}Repository";
+        $this->implementations['app\interfaces\RowsRepositoryInterface'] = "app\\repositories\\{$entity}RowsRepository";
     }
 }
